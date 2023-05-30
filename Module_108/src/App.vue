b7ec5771025890bbc1cd7d581ef44a45dfcf553f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-05-30 21:01:46 +0200

    Finished module 116

--- Module_108/src/App.vue
+++ Module_108/src/App.vue
@@ -3,6 +3,11 @@
         <the-header></the-header>
         <badge-list></badge-list>
         <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+        <course-goals #default="slotProps">
+            <h2>
+                {{ slotProps.item }}
+            </h2>
+        </course-goals>
     </div>
 </template>
 
@@ -10,12 +15,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
     components: {
         TheHeader,
         BadgeList,
         UserInfo,
+        CourseGoals,
     },
     data() {
         return {
