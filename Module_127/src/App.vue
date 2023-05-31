f36dcabbd43babff0bf5bcf730f06e2ba2d96d0f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-05-31 19:48:49 +0200

    Finished module 127

---
+++ Module_127/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources" :key="resource.id">
+            <h2>{{ resource.title }}</h2>
+            <p>{{ resource.description }}</p>
+            <a :href="resource.link">Visit</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Where we find everything else',
+                    link: 'https://google.com'
+                },
+                {
+                    id: 'stackoverflow',
+                    title: 'Stack Overflow',
+                    description: 'Where we find and share knowledge',
+                    link: 'https://stackoverflow.com'
+                }
+            ]
+        };
+    }
+}
+
+</script>
