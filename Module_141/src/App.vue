46fb86b61e4c857ae722392af78dfd1808dacd1d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-01 22:14:17 +0200

    Fixed styling

--- Module_141/src/App.vue
+++ Module_141/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <the-form></the-form>
+    <the-form></the-form>
 </template>
 
 <script>
 import TheForm from './components/TheForm.vue';
 
 export default {
-  components: {
-    TheForm
-  }  
+    components: {
+        TheForm
+    }
 }
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 html {
-  font-family: sans-serif;
+    font-family: sans-serif;
 }
 
 body {
-  margin: 0;
-  background-color: #292929;
+    margin: 0;
+    background-color: #292929;
 }
 </style>
