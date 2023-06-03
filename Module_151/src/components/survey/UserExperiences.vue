cc8ecd7d5073597a7eee22dc1162c94774179227
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-03 14:10:03 +0200

    Finished module 157

--- Module_151/src/components/survey/UserExperiences.vue
+++ Module_151/src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
         <base-card>
             <h2>Submitted Experiences</h2>
             <div>
-                <base-button>Load Submitted Experiences</base-button>
+                <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
             </div>
             <ul>
                 <survey-result v-for="result in results" :key="result.id" :name="result.name"
@@ -20,6 +20,28 @@ export default {
     components: {
         SurveyResult,
     },
+    data() {
+        return {
+            results: [],
+        };
+    },
+    methods: {
+        loadExperiences() {
+            fetch('https://vue-udemy-course-66b64-default-rtdb.europe-west1.firebasedatabase.app/surveys.json')
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then((data) => {
+                    const results = [];
+                    for (const id in data) {
+                        results.push({ id: id, name: data[id].name, rating: data[id].rating });
+                    }
+                    this.results = results;
+                });
+        }
+    }
 };
 </script>
 
