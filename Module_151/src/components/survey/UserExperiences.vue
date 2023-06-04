3133c0205a927d9a286c546f783a67ce20e7a48d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-04 15:12:45 +0200

    Finished module 159

--- Module_151/src/components/survey/UserExperiences.vue
+++ Module_151/src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
             <div>
                 <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
             </div>
-            <ul>
+            <p v-if="isLoading">Loading ...</p>
+            <ul v-else>
                 <survey-result v-for="result in results" :key="result.id" :name="result.name"
                     :rating="result.rating"></survey-result>
             </ul>
@@ -23,10 +24,12 @@ export default {
     data() {
         return {
             results: [],
+            isLoading: false,
         };
     },
     methods: {
         loadExperiences() {
+            this.isLoading = true;
             fetch('https://vue-udemy-course-66b64-default-rtdb.europe-west1.firebasedatabase.app/surveys.json')
                 .then((response) => {
                     if (response.ok) {
@@ -34,6 +37,7 @@ export default {
                     }
                 })
                 .then((data) => {
+                    this.isLoading = false;
                     const results = [];
                     for (const id in data) {
                         results.push({ id: id, name: data[id].name, rating: data[id].rating });
