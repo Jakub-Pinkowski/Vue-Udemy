15c07e73652b327814c69e73f078a42b3694bc04
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-05 20:49:24 +0200

    Finished module 176

--- Module_166/src/main.js
+++ Module_166/src/main.js
@@ -11,7 +11,7 @@ const router = createRouter({
     routes: [
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamsMembers },
+        { path: '/teams/:teamId', component: TeamsMembers, props: true },
     ],
 });
 
