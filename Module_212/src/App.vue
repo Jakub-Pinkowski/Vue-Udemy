c07161473560fd734e934db4a71c543221171bb5
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-11 17:14:11 +0200

    Finished module 113

--- Module_212/src/App.vue
+++ Module_212/src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <base-container title="Vuex">
-  <h3> {{ $store.state.counter }}</h3>
-  <button>Add 1</button>
-  </base-container>
+    <base-container title="Vuex">
+        <the-counter></the-counter>
+        <button @click="AddOne">Add 1</button>
+    </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
-  components: {
-    BaseContainer,
-  },
+    components: {
+        BaseContainer,
+        TheCounter,
+    },
+    methods: {
+        AddOne() {
+            this.$store.state.counter++;
+        },
+    },
 };
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 html {
-  font-family: sans-serif;
+    font-family: sans-serif;
 }
 
 body {
-  margin: 0;
+    margin: 0;
 }
 </style>
--- Module_212/src/main.js
+++ Module_212/src/main.js
@@ -6,9 +6,9 @@ import App from './App.vue';
 const store = createStore({
     state() {
         return {
-            counter: 0
+            counter: 0,
         };
-    }
+    },
 });
 
 const app = createApp(App);
