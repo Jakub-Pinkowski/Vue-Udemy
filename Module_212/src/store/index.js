55800e0702968d78fc3318e03fc479e5027956cf
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-11 19:10:19 +0200

    Finished module 224

---
+++ Module_212/src/store/index.js
@@ -0,0 +1,32 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+    modules: {
+        numbers: counterModule,
+    },
+    state() {
+        return {
+            isLoggedIn: false,
+        };
+    },
+    mutations: {
+        setAuth(state, payload) {
+            state.isLoggedIn = payload.isAuth;
+        },
+    },
+    actions: {
+        login(context) {
+            context.commit('setAuth', { isAuth: true });
+        },
+        logout(context) {
+            context.commit('setAuth', { isAuth: false });
+        },
+    },
+    getters: {
+        userIsAuthenticated(state) {
+            return state.isLoggedIn;
+        },
+    },
+});
+
+export default store;
