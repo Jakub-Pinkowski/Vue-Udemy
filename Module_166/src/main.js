b05021886e57aba72d576d6cf8d1894eab9855da
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-11 11:35:38 +0200

    Finished module 185

--- Module_166/src/main.js
+++ Module_166/src/main.js
@@ -57,6 +57,11 @@ router.beforeEach(function(to, from, next) {
     next();
 });
 
+router.afterEach(function(to, from) {
+    console.log('Global afterEach');
+    console.log(to, from);
+});
+
 const app = createApp(App);
 
 app.use(router);
