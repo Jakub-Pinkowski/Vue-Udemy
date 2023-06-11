6531b4affbf0a9a1572215092623dab7754398f4
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-11 18:37:00 +0200

    Finished module 220

--- Module_212/src/components/ChangeCounter.vue
+++ Module_212/src/components/ChangeCounter.vue
@@ -1,6 +1,6 @@
 <template>
     <button @click="increment">Add 2</button>
-    <button @click="increase({value: 10})">Add 10</button>
+    <button @click="increase({ value: 10 })">Add 10</button>
 </template>
 
 <script>
