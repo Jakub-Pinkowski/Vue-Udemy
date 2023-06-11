2bd74eecb4b6686e15aa3a0d07da8c49dd20c0bd
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-11 11:53:08 +0200

    Finished module 186

--- Module_166/src/components/users/UsersList.vue
+++ Module_166/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
     <button @click="confirmInput">Confirm</button>
+    <button @click="saveChanges">Save Changes</button>
     <ul>
         <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
     </ul>
@@ -13,15 +14,34 @@ export default {
         UserItem,
     },
     inject: ['users'],
+    data() {
+        return {
+            changesSaved: false,
+        };
+    },
     methods: {
         confirmInput() {
             // do something
             this.$router.push('/teams');
         },
+        saveChanges() {
+            this.changesSaved = true;
+        },
     },
-    beforeRouteEnter (to, from, next) {
-        // ...
+    beforeRouteLeave (_, _2, next) {
+        console.log('beforeRouteLeave');
+        if (this.changesSaved) {
+            next();
+        } else {
+            const answer = confirm('Do you want to leave without saving?');
+            if (answer) {
+                next();
+            } else {
+                next(false);
+            }
+        }
     }
+
 };
 </script>
 
