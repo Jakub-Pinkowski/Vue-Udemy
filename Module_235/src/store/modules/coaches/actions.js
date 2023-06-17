525e2fd550ca7672c425656305d39082d3fe5fa5
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-17 21:46:06 +0200

    Finished module 251

--- Module_235/src/store/modules/coaches/actions.js
+++ Module_235/src/store/modules/coaches/actions.js
@@ -1,14 +1,35 @@
 export default {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
         const coachData = {
             id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             hourlyRate: data.rate,
-            areas: data.areas
+            areas: data.areas,
         };
 
-        context.commit('registerCoach', coachData);
-    }
-}
+        const response = await fetch(
+            `https://vue-udemy-section16-default-rtdb.europe-west1.firebasedatabase.app/coaches${userId}.json`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(coachData),
+            }
+        );
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(
+                responseData.message || 'Failed to fetch!'
+            );
+            throw error;
+        }
+
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId,
+        });
+    },
+};
