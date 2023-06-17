685e1fead3eb1ba3f3548babf7dba06654a6a6ea
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-17 22:04:53 +0200

    Finished module 252

--- Module_235/src/store/modules/coaches/actions.js
+++ Module_235/src/store/modules/coaches/actions.js
@@ -11,7 +11,7 @@ export default {
         };
 
         const response = await fetch(
-            `https://vue-udemy-section16-default-rtdb.europe-west1.firebasedatabase.app/coaches${userId}.json`,
+            `https://vue-udemy-section16-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
             {
                 method: 'PUT',
                 body: JSON.stringify(coachData),
@@ -31,5 +31,34 @@ export default {
             ...coachData,
             id: userId,
         });
+    },
+    async loadCoaches(context) {
+        const response = await fetch(
+            `https://vue-udemy-section16-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+        );
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(
+                responseData.message || 'Failed to fetch!'
+            );
+            throw error;
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas,
+            };
+            coaches.push(coach);
+        }
+
+        context.commit('setCoaches', coaches);
     },
 };
