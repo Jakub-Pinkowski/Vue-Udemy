391391de1de9e05f53d8c9783d057f93a3e3d413
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-17 17:26:22 +0200

    Finished module 240

--- Module_235/src/pages/coaches/CoachesList.vue
+++ Module_235/src/pages/coaches/CoachesList.vue
@@ -8,9 +8,15 @@
             <router-link to="/register">Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{  coach.firstName }} {{ coach.lastName }}
-            </li>
+            <coach-item
+                v-for="coach in filteredCoaches"
+                :key="coach.id"
+                :id="coach.id"
+                :firstName="coach.firstName"
+                :lastName="coach.lastName"
+                :areas="coach.areas"
+                :rate="coach.hourlyRate"
+            ></coach-item>
         </ul>
         <h3 v-else>
             No coaches found.
@@ -19,7 +25,12 @@
 </template>
 
 <script>
+import CoachItem from '@/components/coaches/CoachItem.vue';
+
 export default {
+    components: {
+        CoachItem,
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
@@ -31,3 +42,16 @@ export default {
 }
 
 </script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.controls {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
