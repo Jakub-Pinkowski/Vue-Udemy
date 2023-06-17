4bf64250a8bc78a68626b5fcb04be88ab89ad632
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-17 16:13:27 +0200

    Finished module 237

--- Module_235/src/App.vue
+++ Module_235/src/App.vue
@@ -1,6 +1,18 @@
 <template>
+
     <router-view></router-view>>
 </template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+
+export default {
+    components: {
+        TheHeader,
+    },
+};
+
+</script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
@@ -17,62 +29,4 @@ body {
   margin: 0;
 }
 
-header {
-  width: 100%;
-  height: 5rem;
-  background-color: #3d008d;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-header a {
-  text-decoration: none;
-  color: #f391e3;
-  display: inline-block;
-  padding: 0.75rem 1.5rem;
-  border: 1px solid transparent;
-}
-
-a:active,
-a:hover,
-a.router-link-active {
-  border: 1px solid #f391e3;
-}
-
-h1 {
-  margin: 0;
-}
-
-h1 a {
-  color: white;
-  margin: 0;
-}
-
-h1 a:hover,
-h1 a:active,
-h1 a.router-link-active {
-  border-color: transparent;
-}
-
-header nav {
-  width: 90%;
-  margin: auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-header ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-li {
-  margin: 0 0.5rem;
-}
 </style>
