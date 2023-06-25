325652f755cf49d7f32f817eeb059b4e626e95f2
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-25 18:10:42 +0200

    Introduced transitions between pages

--- Module_235/src/pages/coaches/CoachesList.vue
+++ Module_235/src/pages/coaches/CoachesList.vue
@@ -1,28 +1,30 @@
 <template>
-    <base-dialog :show="!!error" @close="handleError" title="An error occured">
-        <p>{{ error }}</p>
-    </base-dialog>
-    <section>
-        <coach-filter @change-filter="setFilter"></coach-filter>
-    </section>
-    <section>
-        <base-card>
-            <div class="controls">
-                <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-                <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
-            </div>
-            <div v-if="isLoading">
-                <base-spinner></base-spinner>
-            </div>
-            <ul v-else-if="hasCoaches">
-                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :firstName="coach.firstName"
-                    :lastName="coach.lastName" :areas="coach.areas" :rate="coach.hourlyRate"></coach-item>
-            </ul>
-            <h3 v-else>
-                No coaches found.
-            </h3>
-        </base-card>
-    </section>
+    <div>
+        <base-dialog :show="!!error" @close="handleError" title="An error occured">
+            <p>{{ error }}</p>
+        </base-dialog>
+        <section>
+            <coach-filter @change-filter="setFilter"></coach-filter>
+        </section>
+        <section>
+            <base-card>
+                <div class="controls">
+                    <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
+                    <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
+                </div>
+                <div v-if="isLoading">
+                    <base-spinner></base-spinner>
+                </div>
+                <ul v-else-if="hasCoaches">
+                    <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :firstName="coach.firstName"
+                        :lastName="coach.lastName" :areas="coach.areas" :rate="coach.hourlyRate"></coach-item>
+                </ul>
+                <h3 v-else>
+                    No coaches found.
+                </h3>
+            </base-card>
+        </section>
+    </div>
 </template>
 
 <script>
@@ -79,7 +81,7 @@ export default {
         async loadCoaches(refresh = false) {
             this.isLoading = true;
             try {
-                await this.$store.dispatch('coaches/loadCoaches', {forceRefresh: refresh});
+                await this.$store.dispatch('coaches/loadCoaches', { forceRefresh: refresh });
             } catch (error) {
                 this.error = error.message || 'Someting went wrong';
             }
--- Module_235/src/pages/requests/RequestReceived.vue
+++ Module_235/src/pages/requests/RequestReceived.vue
@@ -1,24 +1,26 @@
 <template>
-    <base-dialog :show="!!error" @close="handleError" title="An error occured">
-        <p>{{ error }}</p>
-    </base-dialog>
-    <section>
-        <base-card>
-            <header>
-                <h2>Requests Received</h2>
-            </header>
-            <div v-if="isLoading">
-                <base-spinner></base-spinner>
-            </div>
-            <ul v-else-if="hasRequests && !isLoading">
-                <request-item v-for="request in receivedRequests" :key="request.id" :email="request.userEmail"
-                    :message="request.message"></request-item>
-            </ul>
-            <h3 v-else>
-                You have no requests yet.
-            </h3>
-        </base-card>
-    </section>
+    <div>
+        <base-dialog :show="!!error" @close="handleError" title="An error occured">
+            <p>{{ error }}</p>
+        </base-dialog>
+        <section>
+            <base-card>
+                <header>
+                    <h2>Requests Received</h2>
+                </header>
+                <div v-if="isLoading">
+                    <base-spinner></base-spinner>
+                </div>
+                <ul v-else-if="hasRequests && !isLoading">
+                    <request-item v-for="request in receivedRequests" :key="request.id" :email="request.userEmail"
+                        :message="request.message"></request-item>
+                </ul>
+                <h3 v-else>
+                    You have no requests yet.
+                </h3>
+            </base-card>
+        </section>
+    </div>
 </template>
 
 <script>
