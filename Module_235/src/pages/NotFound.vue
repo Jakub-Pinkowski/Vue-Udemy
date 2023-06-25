8f9470ffbc430cd7fb60d1139b458c61bbc63db8
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-25 18:12:27 +0200

    Introduced NotFound routing

--- Module_235/src/pages/NotFound.vue
+++ Module_235/src/pages/NotFound.vue
@@ -1,3 +1,6 @@
 <template>
-    NOT FOUND
+    <base-card>
+        <h2>Page not found!</h2>
+        <p>Sorry, this page does not exist. <router-link to="coaches">Coaches</router-link></p>
+    </base-card>
 </template>
