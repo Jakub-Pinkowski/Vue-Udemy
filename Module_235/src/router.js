6c57d5143d539f3d139c8c55804b2a404db35469
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-26 21:11:30 +0200

    Finished module 278

--- Module_235/src/router.js
+++ Module_235/src/router.js
@@ -1,13 +1,30 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestReceived from './pages/requests/RequestReceived.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestReceived from './pages/requests/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = defineAsyncComponent(() =>
+    import('./pages/coaches/CoachDetail.vue')
+);
+const CoachRegistration = defineAsyncComponent(() =>
+    import('./pages/coaches/CoachRegistration.vue')
+);
+const ContactCoach = defineAsyncComponent(() =>
+    import('./pages/requests/ContactCoach.vue')
+);
+const RequestReceived = defineAsyncComponent(() =>
+    import('./pages/requests/RequestReceived.vue')
+);
+const UserAuth = defineAsyncComponent(() =>
+    import('./pages/auth/UserAuth.vue')
+);
 
 const router = createRouter({
     history: createWebHistory(),
@@ -44,10 +61,7 @@ const router = createRouter({
 router.beforeEach((to, _, next) => {
     if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
         next('/auth');
-    } else if (
-        to.meta.requiresUnauth &&
-        store.getters.isAuthenticated
-    ) {
+    } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
         next('/coaches');
     } else {
         next();
