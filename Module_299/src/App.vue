282c73c03acdb25bb2930f9e51e30553d7492c28
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-06-29 19:17:06 +0200

    Finished module 300

--- Module_299/src/App.vue
+++ Module_299/src/App.vue
@@ -5,29 +5,20 @@
     </main>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 import USER_DATA from './dummy-data.js';
 
 import UserList from './components/users/UserList.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
 
-export default {
-    components: {
-        UserList,
-        ProjectsList,
-    },
-    data() {
-        return {
-            selectedUser: null,
-            activeUsers: USER_DATA,
-        };
-    },
-    methods: {
-        selectUser(uid) {
-            this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-        },
-    },
-};
+const selectedUser = ref(null);
+const activeUsers = USER_DATA
+
+function selectUser(uid) {
+    selectedUser.value = activeUsers.find((u) => u.id === uid);
+}
+
 </script>
 
 <style>
@@ -66,4 +57,5 @@ button:active {
 button.selected {
     background-color: #00006b;
     color: white;
-}</style>
+}
+</style>
