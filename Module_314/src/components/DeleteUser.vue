a4e27cb6072607eebe6486cbca905af2aabd0da8
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-07-01 18:55:52 +0200

    Finished module 314

--- Module_314/src/components/DeleteUser.vue
+++ Module_314/src/components/DeleteUser.vue
@@ -1,36 +1,29 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
-    <p>Do you want to continue with deleting a user?</p>
-  </user-alert>
-  <section>
-    <h2>Delete a User</h2>
-    <button @click="showAlert">Delete User</button>
-  </section>
+    <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+        <p>Do you want to continue with deleting a user?</p>
+    </user-alert>
+    <section>
+        <h2>Delete a User</h2>
+        <button @click="showAlert">Delete User</button>
+    </section>
 </template>
 
 <script>
-import { ref } from 'vue';
 import UserAlert from './UserAlert.vue';
+import useAlert from '../hooks/alert.js';
 
 export default {
-  components: {
-    UserAlert,
-  },
-  setup() {
-    const alertIsVisible = ref(false);
+    components: {
+        UserAlert,
+    },
+    setup() {
+        const [alertIsVisible, showAlert, hideAlert] = useAlert();
 
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
-
-    return {
-      alertIsVisible,
-      showAlert,
-      hideAlert,
-    };
-  },
+        return {
+            alertIsVisible,
+            showAlert,
+            hideAlert,
+        };
+    },
 };
 </script>
