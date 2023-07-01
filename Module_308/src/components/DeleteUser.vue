b99021e0d220c385ddfc5278cc72e10412d19c05
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-07-01 18:15:29 +0200

    Finished module 311

--- Module_308/src/components/DeleteUser.vue
+++ Module_308/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-    <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+    <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
         <p>Do you want to continue with deleting a user?</p>
     </user-alert>
     <section>
@@ -14,6 +14,11 @@ import alertMixin from '../mixins/alert.js';
 
 export default {
     mixins: [alertMixin],
+    data() {
+        return {
+            alertTitle: 'Delete the User?',
+        };
+    },
     components: {
         UserAlert,
     },
