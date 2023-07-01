4314648ea6e7986cdf7500f1cc718bc40f5dfbcd
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-07-01 20:12:22 +0200

    Finished module 319

--- Module_317/src/components/projects/ProjectsList.vue
+++ Module_317/src/components/projects/ProjectsList.vue
@@ -33,14 +33,14 @@ export default {
         const { enteredSearchTerm, availableItems, updateSearch, } = useSearch(projects, 'title');
 
         const hasProjects = computed(function () {
-            return props.user.projects && availableItems.value.length > 0;
+            return user.value.projects && availableItems.value.length > 0;
         });
 
         // const propsWithRefs = toRefs(props);
         // const user = propsWithRefs.user;
 
         watch(user, function () {
-            enteredSearchTerm.value = '';
+            updateSearch('')
         });
 
         return {
